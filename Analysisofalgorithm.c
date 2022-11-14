48a5a9664987aa042117c95da61c9dd680050338
roshawn wright <89256902+Rwright7@users.noreply.github.com>
2022-11-14 00:10:07 -0500

    Update Analysisofalgorithm.c

--- Analysisofalgorithm.c
+++ Analysisofalgorithm.c
@@ -1,234 +1,170 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<errno.h>
 
 
-int menu();
-void pauseConsole();
-void Bubble_Sort();
-void Quick_Sort();
-void Selection_Sort();
-void Heap_Sort();
-void Insertion_Sort();
-void Merge_Sort();
+#define SIZEOF_ARRAY(x) (sizeof(x)/sizeof(*(x)))
 
-int main()
+
+enum Choice {
+	CHOICE_INVALID = -1,
+	CHOICE_DONT_SORT,
+	CHOICE_BUBBLE_SORT,
+	CHOICE_QUICK_SORT,
+	CHOICE_SELECTION_SORT,
+	CHOICE_INSERTION_SORT,
+	CHOICE_MERGE_SORT,
+	CHOICE_HEAP_SORT,
+};
+
+void array_randomize(int len, int arr[len], int maxValue)
 {
-	int choice;
-
-	system("clear");
-	do
+	for (int i = 0; i < len; ++i)
 	{
-		choice = menu();
+		arr[i] = rand() % maxValue;
+	}
+}
+
+void array_print(int len, const int arr[len])
+{
+	printf("%d: [", len);
+	for (int i = 0; i < (len - 1); ++i)
+	{
+		printf("%d,", arr[i]);
+	}
+	printf("%d]\n", arr[len - 1]);
+}
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+time_t time_millisec(void)
+{
+	struct timespec time = {0};
+	clock_gettime(CLOCK_MONOTONIC, &time);
+	return (time.tv_sec. * 1000000ULL) + (time.tv_nsec / 1000ULL);
+}
+
+
+enum Choice menu(void);
+
+void bubble_sort(int len, int arr[len]);
+void quick_sort(int len, int arr[len]);
+void selection_sort(int len, int arr[len]);
+void insertion_Sort(int len, int arr[len]);
+void merge_sort(int len, int arr[len]);
+void heap_sort(int len, int arr[len]);
+
+
+#define ARR_SIZE 10
+
+int main(void)
+{
+	srand((unsigned)time(NULL));
+	
+	int testArray[ARR_SIZE] = { 0 };
+	enum Choice choice = 0; 
+	
+	do {
+		choice = menu ();
 		system("clear");
-		switch(choice)
-		{
-			case 1:
-				Bubble_Sort();
-				break;
-			case 2:
-				Quick_Sort();
-				break;
-			case 3:
-				Selection_Sort();
-				break;
-			case 4:
-				Heap_Sort();
-				break;
-			case 5:
-				Insertion_Sort();
-				break;
-			case 6:
-				Merge_Sort();
-				break;
-			case 7:
-				printf("\n\n....goodbye!....\n\n");
-				exit(0);
-			default:
-				printf("Incorrect Selection!!");
-		}
+		
+		if (choice != CHOICE_INVALID && choice != CHOICE_DONT_SORT) {
+           	 array_randomize(SIZEOF_ARRAY(testArray), testArray, 10000);
+            	 printf("============ Unsorted array: ============\n");
+            	 array_print(SIZEOF_ARRAY(testArray), testArray);
+            	 printf("\n");
+        }   
 
-	} while(choice!=6);
-	return 0;
-}
-
-
-int menu()
-{
-	int choice;
-	printf(" ============ SORTING ALGORITHMS WITH RUNTIME ============\n");
-	printf("Choose an Option: \n1. Bubble Sort \n2. Quick Sort \n3. Selection Sort \n4. Heap Sort \n5. Insertion Sort \n6. Merge Sort \n7. Exit \n");
-	printf(" ============ SORTING ALGORITHMS WITH RUNTIME ============\n");
-	scanf("%d", &choice);
-	return choice;
-
-}
-
-void pauseConsole(void)
-{
-	fputs("Press <ENTER> to continue...", stdout);
-	getchar();
-}
-
-void Bubble_Sort()
-{
-	int arr1[15000];
-	int arr2[65000];
-	int arr3[150000];
-
-	clock_t begin, end;  //data type for calculating time
-	double time1,time2, time3;  //variable for storing total time of execution for each dataset
-	int i, y = 0, temp;
-
-	printf("\n============ Unsorted array dataset ============ 1:\n"); //populating array with dataset 1
-	for(i = 0; i < 15000; i++)
+	switch (choice)
 	{
+	case CHOICE_BUBBLE_SORT:
+            bubble_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_QUICK_SORT:
+            quick_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_SELECTION_SORT:
+            selection_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_INSERTION_SORT:
+            insertion_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_MERGE_SORT:
+            merge_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_HEAP_SORT:
+            heap_sort(SIZEOF_ARRAY(testArray), testArray);
+            break;
+        case CHOICE_DONT_SORT:
+            break;
 
-		for(i = 0; i < 15000; i++) //displaying unsorted array
+        case CHOICE_INVALID:
+        default:
+            printf("Invalid selection!\n\n");
+        }
+    } while (choice != CHOICE_DONT_SORT);
+
+    printf("....goodbye!....\n\n");
+
+    return 0;
+}
+		
+enum Choice menu(void)
+{
+	printf("  ============ SORTING ALGORITHMS WITH RUNTIME ============\n");
+	printf("Choose an option:\n");
+	
+	printf("%d. Exit\n", CHOICE_DONT_SORT);
+	printf("%d. Bubble sort\n", CHOICE_BUBBLE_SORT);
+	printf("%d. Quick sort\n", CHOICE_QUICK_SORT);
+	printf("%d. Selection sort\n", CHOICE_SELECTION_SORT);
+	printf("%d. Insertion sort\n", CHOICE_INSERTION_SORT);
+	printf("%d. Merge sort\n", CHOICE_MERGE_SORT);
+	printf("%d. Heap sort\n", CHOICE_HEAP_SORT);
+	
+	printf(" ============ SORTING ALGORITHMS WITH RUNTIME ============\n");
+	printf(">>>> ");
+	
+	char buff[64] = { 0 };
+	fgets(buff, sizeof (buff), stdin);
+	
+	char *endptr = 	NULL; 
+	errno = 0; 
+	int x = (int)strtol(buff, &endptr, 10);
+	if (errno != 0 || endptr == buff){
+		return CHOICE_INVALID;
+	}
+	return x; 
+}
+		
+void Bubble_Sort(const int len, int arr[len])
+{
+	time_t begin = time_millisec();
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < (len - 1 - i); ++j)
 		{
-
-			arr1[i] = rand();  //populating array with random numbers
-			printf("\n============ Index %d::Element %d ============\n", i, arr1[i]);
-		}
-		pauseConsole();
-		//bubble sort begins
-		begin = clock(); //variable to mark the starting time of sorting
-		printf("\n\n============ Dataset 1 Clock started at: %f ============\n", (float)begin);
-
-		for(i = 0; i <= 15000 - 2; i++)
-		{
-			for(y = 0; y <= 15000 - 1 - i; y++)
-			{
-				if(arr1[y] > arr1[y+1])
-				{
-					temp = arr1[y];
-					arr1[y] = arr1[y+1];
-					arr1[y+1] = temp;
-				}
-
+			if (arr[j] > arr[j + 1]){
+				swap_int(&arr[j], &arr[j + 1]);
 			}
 		}
-		end = clock();
-		pauseConsole();	
-		printf("\n============ Dataset 1 Clock ended at: %f ============\n", (float)end);
-		time1 = ((double)(end - begin)) / CLOCKS_PER_SEC;
-		printf("\n============ Total execution time for dataset 1 is:%f ============\n", time1);
-		pauseConsole();
-
-		//Sorted array
-		printf("\n============ Sorted array dataset 1 is: ============\n");
-		pauseConsole();
-		for(i = 0; i < 15000; i++)
-		{
-			printf("\n============ Index %d::Element %d ============\n",i, arr1[i]);
-		}	
 	}
+	time_t end = time_millisec();
+	
+	 printf("============ Sorted array: ============\n");
+         array_print(len, arr);
+         printf("\n");
 
-	pauseConsole();
-	//populating arrays with dataset 2
-	printf("\n============ Unsorted array dataset 2 is: ============\n");
-	pauseConsole();
-	for(i = 0; i < 65000; i++)
-	{
-		//displaying unsorted array
-		for(i = 0; i < 65000; i++)
-		{
-			//populating array with random numbers
-			arr2[i] = rand();
-			printf("\n============ Index %d::Element %d ============\n", i, arr2[i]);
-		}
-		pauseConsole();
-		//bubble sort begins
-		begin = clock();//variable to mark the starting time of sorting
-		printf("\n\n============ Dataset 2 Clock started at:%f =============\n", (float)begin);
-		pauseConsole();	
-		for(i = 0; i < 65000; i++)
-		{
-
-			for(y = 0; y <= 65000 - 1 - i; y++)
-			{
-				if(arr2[y] > arr2[y+1])
-				{
-					temp = arr2[y];
-					arr2[y] = arr2[y+1];
-					arr2[y+1] = temp;
-				}
-
-			}		
-
-		}
-		end = clock();
-		//system("\npause");	
-		printf("\n============= Dataset 2 Clock ended at:%f ============\n", (float)end);
-		time2 = ((double)(end - begin)) / CLOCKS_PER_SEC;
-		printf("\n============ Total execution time for dataset 2 is:%f ============\n", time2);
-		pauseConsole();
-
-		//Sorted array
-		printf("\n");
-		printf("============ Sorted array dataset 2 is:: ============\n");
-		pauseConsole();
-		for(i = 0; i < 65000; i++)
-		{
-			printf("\n============ Index %d::Element %d ============\n",i, arr2[i]);
-		}	
-	}
-	pauseConsole();
-
-	//populating arrays with dataset 3
-	printf("\n============ Unsorted array dataset 3 is: ============\n");
-	pauseConsole();
-	for(i = 0; i < 150000; i++)
-	{
-		//displaying unsorted array
-		for(i = 0; i < 150000; i++)
-		{
-			//populating array with random numbers
-			arr3[i] = rand();
-			printf("\n============ Index %d::Element %d ============\n", i, arr3[i]);
-		}
-		pauseConsole();
-		//bubble sort begins
-		begin = clock();//variable to mark the starting time of sorting
-		printf("\n============ Dataset 3 Clock started at:%f =============\n", (float)begin);	
-		for(i = 0; i < 150000; i++)
-		{
-			//min=i;
-			for(y = 0; y <= 150000 - 1 - i; y++)
-			{
-				if(arr3[y] > arr3[y+1])
-				{
-					temp = arr3[y];
-					arr3[y] = arr3[y+1];
-					arr3[y+1] = temp;	
-				}
-
-			}		
-
-		}
-		end = clock();
-		pauseConsole();	
-		printf("\n============ Dataset 3 Clock ended at: %f ============\n", (float)end);
-		time3 = ((double)(end - begin)) / CLOCKS_PER_SEC;
-		printf("\n============ Total execution time for dataset 3 is: %f ============\n", time3);
-		pauseConsole();
-		//Sorted array
-		printf("\n============ Sorted array dataset 3 is:: ============\n");
-		pauseConsole();
-		for(i = 0; i < 150000; i++)
-		{
-			printf("\n============ Index %d::Element %d ============\n\n",i, arr3[i]);
-		}
-	}
-
+    	printf("On array of %d elements, Bubble Sort took %ld[ms].\n", len, end - begin);
 }
-void swap(int *a, int *b);
-int part(int arr1[], int low, int high);
-int part_two(int arr2[], int low, int high);
-int part_three(int arr3[], int low, int high);
-void qSort(int arr1, int low, int high);
-void qSort_two(int arr2, int low, int high);
-void qSort_three(int arr3, int low, int high);
+
 
 void swap(int *a, int *b)
 {
